c987717414588ca186774fd2ae08719c3ac9a4d5
Rimvydas Šimkus <84323090+Rimvydas1987@users.noreply.github.com>
2021-06-17 09:20:55 +0300

    pataisa

--- 3-uzduotis/script.js
+++ 3-uzduotis/script.js
@@ -1,36 +1,15 @@
 "use strict";
 
 
-function trikampis() {
+function trikampis(){
+    let a, b, c;
+    a = document.getElementById("reiksme_A").value;
+    b = document.getElementById("reiksme_B").value;
+    c = document.getElementById("reiksme_C").value;
 
-var A, B, C;
-
-A = document.getElementById("reiksme_A").value;
-B = document.getElementById("reiksme_B").value;
-C = document.getElementById("reiksme_C").value;
-
-
-if(A <= 0){
-    A = 0;
-} else {
-    A = 1;
+    if (a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0){
+        document.getElementById("Atsakymas").innerHTML = "Trikampis gaunasi";
+    } else {
+        document.getElementById("Atsakymas").innerHTML = "Trikampis nesigauna"; 
+    }   
 }
-
-if(B <= 0){
-    B = 0;
-} else {
-    B = 1;
-}
-
-if(C <= 0){
-    C = 0;
-} else {
-    C = 1;
-}
-
-if(A + B + C == 3){
-    document.getElementById("Atsakymas").innerHTML = "Trikampis gaunasi";
-} else {
-    document.getElementById("Atsakymas").innerHTML = "Trikampis nesigauna";
-}
-}  
